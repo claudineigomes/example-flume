d689b3983f333c172708d451a20c9034098212ee
paliwalashish <paliwalashish@gmail.com>
2014-04-28 18:11:32 +0530

    added replication channel initial draft

---
+++ src/main/java/com/ashishpaliwal/flume/channels/ReplicatingChannel.java
@@ -0,0 +1,11 @@
+package com.ashishpaliwal.flume.channels;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ashishpaliwal
+ * Date: 28/04/14
+ * Time: 3:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReplicatingChannel {
+}
---
+++ src/test/java/com/ashishpaliwal/flume/channels/TestReplicatingChannel.java
@@ -0,0 +1,11 @@
+package com.ashishpaliwal.flume.channels;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ashishpaliwal
+ * Date: 28/04/14
+ * Time: 5:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestReplicatingChannel {
+}
